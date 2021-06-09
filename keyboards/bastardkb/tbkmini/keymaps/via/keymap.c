3c4f47fe2d8b844206865bcd279ef9eab4313af2
Derek Nheiley <derek.nheiley@gmail.com>
2021-06-08 23:54:45 -0400

    oh, and one more thing...

--- keyboards/bastardkb/tbkmini/keymaps/via/keymap.c
+++ keyboards/bastardkb/tbkmini/keymaps/via/keymap.c
@@ -64,7 +64,7 @@ enum combos {
   JK_LPRN,
   KL_RPRN,
 
-  XC_BSPC,
+  XC_DEL,
   CV_ESC,
 
   MCOMM_MINS,
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY_LAYER] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSLS,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+  //|--------+------1-+------2-+------3-+------4-+------5-|                    |------6-+------7-+------8-+------9-+-----0--+--------|
 LSFT_T(KC_CAPS),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, RSFT_T(KC_QUOT),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
  KC_LCPO, LALT_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RCPC,
