996eefddebd70fc9a3d8677f03097dc5ea34622a
Derek Nheiley <derek.nheiley@gmail.com>
2021-11-06 20:16:08 -0400

    update dactyl_promicro to work with latest vial

---
+++ keyboards/handwired/dactyl_promicro/keymaps/via/rules.mk
@@ -0,0 +1,11 @@
+VIAL_ENABLE = yes
+VIAL_INSECURE = yes
+VIA_ENABLE = yes
+RGBLIGHT_ENABLE = yes
+COMBO_ENABLE = yes
+
+# turn things off to save space
+EXTRAFLAGS += -flto
+COMMAND_ENABLE   = no # Commands for debug and configuration
+CONSOLE_ENABLE   = no # Console for debug
+MOUSEKEY_ENABLE  = no # Mouse keys(+4700)
